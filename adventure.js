b3fcbd22b89efc4cf4dc260936ba23bb93e46e5c
Kaylee <99057337@mydavinci.nl>
2020-12-14 17:40:41 +0100

    images toevoegen en stylen

--- adventure.js
+++ adventure.js
@@ -6,18 +6,21 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 
-// firstImage.src = "images/begin.jpg";
 
 title.innerText = "Cabin in the snow";
 description.innerText = "Welcome to my adventure game";
 button1.innerText = "Begin";
 button2.style.display = "none";
 button3.style.display = "none";
-inventoryItem.src = "images/begin.jpg";
+// inventoryItem.src = "images/mountain.jpg";
+// document.body.style.backgroundImage = "url('images/mountain.jpg')";
+// document.body.style.backgroundSize = "cover";
 
 
 button1.onclick = function(){
-	inventoryItem.src = "images/cabin.jpg";
+	document.body.style.backgroundImage = "url('images/cabin2.jpg')";
+	document.body.style.backgroundSize = "cover";
+	inventoryItem.src = "images/cabin2.jpg";
 	button1.innerText = "Rechts";
 	button2.style.display = "";
 	button2.innerText = "links";
