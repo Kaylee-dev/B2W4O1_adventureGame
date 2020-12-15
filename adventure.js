2cf9ec8b3f461dc4300eccb498ef4a6674521aee
Kaylee <99057337@mydavinci.nl>
2020-12-15 15:33:05 +0100

    eerste functions toegevoegd

--- adventure.js
+++ adventure.js
@@ -6,23 +6,58 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 
+var inventory = {
+	weapon: false,
+	magic: false,
+	coins: 10
+};
+console.log(inventory);
 
-title.innerText = "Cabin in the snow";
-description.innerText = "Welcome to my adventure game";
-button1.innerText = "Begin";
+// inventory.weapon = {
+// 	name: "zwaard",
+// 	strength: 1
+// }
+// console.log(inventory.weapon);
+
+title.innerText = "Snow";
+description.innerText = "Welkom bij mijn adventure game";
+button1.innerText = "Start";
 button2.style.display = "none";
 button3.style.display = "none";
-// inventoryItem.src = "images/mountain.jpg";
-// document.body.style.backgroundImage = "url('images/mountain.jpg')";
-// document.body.style.backgroundSize = "cover";
 
+button1.onclick = begin;
 
-button1.onclick = function(){
-	document.body.style.backgroundImage = "url('images/cabin2.jpg')";
+function begin(){
+	document.body.style.backgroundImage = "url('images/mountain.jpg')";
 	document.body.style.backgroundSize = "cover";
-	inventoryItem.src = "images/cabin2.jpg";
-	button1.innerText = "Rechts";
+	description.innerText = "Je word wakker op een groot veld in de sneeuw. Je kan 3 kanten op gaan, welke kies je?";
+	button1.innerText = "Links";
+	button1.onclick = left;
 	button2.style.display = "";
-	button2.innerText = "links";
+	button2.innerText = "Volg het pad";
+	button2.onclick = "";
+	button3.style.display = "";
+	button3.innerText = "Rechts";
+	button3.onclick = right;
+}
 
+function left(){
+	// document.body.style.backgroundImage = "url('images/')"
+	description.innerText = "Je loopt een stuk naar links en het word enorm mistig. Je kan niks meer zien en je moet terug lopen.";
+	button1.innerText = "Back";
+	button1.onclick = begin;
+	button2.style.display = "none";
+	button3.style.display = "none";
+}
+
+function right(){
+	description.innerText = "Je loopt naar rechts en kan daar niet verder. Er zijn enorme bergen waar je niet overheen kan. Je moet terug lopen.";
+	button1.innerText = "Back";
+	button1.onclick = begin;
+	button2.style.display = "none";
+	button3.style.display = "none";
+}
+
+function path(){
+	description.innerText = "Je volgt het pad en komt aan bij een dorpje."
 }
